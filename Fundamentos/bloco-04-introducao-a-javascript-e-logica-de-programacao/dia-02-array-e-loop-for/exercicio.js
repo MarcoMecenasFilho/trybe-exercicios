5be2d8ff622c277b5b63bd24f5b1f821db77beee
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-07-29 18:16:48 -0300

    adiciona exercicos bloco 4 dia 2

--- Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio.js
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio.js
@@ -50,3 +50,49 @@ console.log(maiornumero);
 
 // Descubra quantos valores ímpares existem no array e imprima o resultado. 
 // Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
+}
+
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
+}
+
+// Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menornumero = numbers[0]
+
+for (index=1;index<numbers.length;index+=1){
+  if (numbers[index] < menornumero) {
+    menornumero = numbers[index]
+  }
+}
+
+console.log(menornumero);
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado.
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+
+console.log(numbers);
+
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index] / 2);
+};
